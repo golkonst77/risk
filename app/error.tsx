82a3535a47583d95d3f9e947742557e45c06fdec
golkonst77 <golkonst@gmail.com>
2025-12-16 13:38:24 +0300

    Fix absolute root links to be basePath-aware

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect } from 'react'
+import Link from 'next/link'
 
 export default function Error({
   error,
@@ -31,15 +32,15 @@ export default function Error({
             >
               Попробовать снова
             </button>
-            <a
+            <Link
               href="/"
               className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               На главную
-            </a>
+            </Link>
           </div>
         </div>
       </div>
     </div>
   )
-} 
+}
